db53b7587d45cb838ea16d247d41c87755b93daa
Joao Batista <joao.batista.dan@gmail.com>
2016-03-30 13:08:46 -0300

    Primeiro teste

---
+++ Projeto 3Asin/src/To/AlunoTO.java
@@ -0,0 +1,69 @@
+package To;
+
+public class AlunoTO {
+	private int matricula;
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String email;
+	private String rg;
+	private String cpf;
+	private String login;
+	private String senha;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRg(){
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf(){
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getLogin(){
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha(){
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
